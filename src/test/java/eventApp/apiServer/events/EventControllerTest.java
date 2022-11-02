85ae3caa251f3159a4e2a363f33a25027c27330c
Sebastiano Secchiaroli <sebssekk@hotmail.it>
2022-11-02 09:28:25 +0100

    Unit Tests for CRUD operations (Controller - Service)

--- src/test/java/eventApp/apiServer/events/EventControllerTest.java
+++ src/test/java/eventApp/apiServer/events/EventControllerTest.java
@@ -1,2 +1,226 @@
-package eventApp.apiServer.events;public class EventControllerTest {
+package eventApp.apiServer.events;
+
+import eventApp.apiServer.events.errors.EventNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebMvcTest
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+public class EventControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private EventService eventService;
+
+    private List<Event> mockEvs;
+
+    @BeforeEach
+    public void setUp(){
+        this.mockEvs = new ArrayList<>(List.of(
+                Event.builder()
+                        .id(1L)
+                        .author("Ben")
+                        .title("Error in server room")
+                        .place("Server Room")
+                        .description("An error occurred in server room and everython g is burning now. I'm super scared about it, need help!")
+                        .dateTime(LocalDateTime.of(2022,12,2,12,23))
+                        .severity(Severity.ALARM)
+                        .build(),
+                Event.builder()
+                        .id(2L)
+                        .author("John")
+                        .title("Clean completed")
+                        .place("2nd floor")
+                        .description("Cleaning operation was successfully completed at second floor, now moving at 3rd")
+                        .dateTime(LocalDateTime.of(2022,10,1,9,17))
+                        .severity(Severity.SUCCESS)
+                        .build()
+        ));
+    }
+    @Test
+    public void should_get_all_events() throws Exception {
+        Mockito.when(eventService.getEvents()).thenReturn(mockEvs);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/api/events")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers
+                        .status()
+                        .isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(2))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].id").value(1l));
+
+    }
+
+    @Test
+    public void should_get_one_event() throws Exception {
+        Mockito.when(eventService.getEvent(1l)).thenReturn(mockEvs.get(0));
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/api/events/1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers
+                        .status()
+                        .isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1l))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.author").value("Ben"));
+    }
+
+    @Test
+    public void should_not_find_one_event() throws Exception {
+        Mockito.when(eventService.getEvent(3l)).thenThrow(EventNotFoundException.class);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/api/events/3")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers
+                        .status()
+                        .isNotFound())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.status").value("NOT_FOUND"));
+    }
+    @Test
+    public void should_add_an_event() throws Exception {
+        Mockito.when(eventService.addEvent(Mockito.any(Event.class))).thenReturn(mockEvs.get(0));
+
+        String json = "{\n" +
+                "  \"title\": \"Error in server roo\",\n" +
+                "  \"description\": \"An error occurred in server room and everython g is burning now. I'm super scared about it, need help!\",\n" +
+                "  \"dateTime\": \"2022-12-02T12:23:00\",\n" +
+                "  \"place\": \"Server Room\",\n" +
+                "  \"severity\":{\"value\":\"ALARM\"},\n" +
+                "  \"author\": \"Ben\"\n" +
+                "}";
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/api/events/add")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(MockMvcResultMatchers
+                        .status()
+                        .isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1l));
+    }
+    @Test
+    public void should_add_all_events() throws Exception {
+        Mockito.when(eventService.addEvents(Mockito.anyList())).thenReturn(mockEvs);
+
+        String json = "[" +
+                "{\n" +
+                "  \"title\": \"Error in server room\",\n" +
+                "  \"description\": \"An error occurred in server room and everython g is burning now. I'm super scared about it, need help!\",\n" +
+                "  \"dateTime\": \"2022-12-02T12:23:00\",\n" +
+                "  \"place\": \"Server Room\",\n" +
+                "  \"severity\": {\"value\":\"ALARM\"},\n" +
+                "  \"author\": \"Ben\"\n" +
+                "},\n" +
+                "{\n" +
+                "  \"title\": \"Clean completed\",\n" +
+                "  \"description\": \"Cleaning operation was successfully completed at second floor, now moving at 3rd\",\n" +
+                "  \"dateTime\": \"2022-10-01T09:17:00\",\n" +
+                "  \"place\": \"2nd floor\",\n" +
+                "  \"severity\": {\"value\": \"SUCCESS\"},\n" +
+                "  \"author\": \"John\"\n" +
+                "}" +
+                "]";
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/api/events/batchadd")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(MockMvcResultMatchers
+                        .status()
+                        .isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(2));
+    }
+
+    @Test
+    public void should_delete_event() throws Exception {
+        Mockito.when(eventService.deleteEvent(1l)).thenReturn(mockEvs.get(0));
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .delete("/api/events/1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers
+                        .status()
+                        .isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1l))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.author").value("Ben"));
+    }
+    @Test
+    public void should_not_delete_event_that_does_not_exist() throws Exception {
+        Mockito.when(eventService.deleteEvent(3l)).thenThrow(EventNotFoundException.class);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .delete("/api/events/3")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers
+                        .status()
+                        .isNotFound());
+    }
+
+    @Test
+    void should_update_an_event() throws Exception {
+        Event updatedEvent = Event.builder()
+                .id(1L)
+                .author("Susan")
+                .title("Error in server room Again")
+                .place("Server Room")
+                .description("An error occurred in server room and everything is burning now. I'm super scared about it, need help!")
+                .dateTime(LocalDateTime.of(2022,12,2,15,23))
+                .severity(Severity.ALARM)
+                .build();
+
+        Mockito.when(eventService.updateEvent(Mockito.eq(1l),Mockito.any(Event.class))).thenReturn(updatedEvent);
+
+        String json = "{\n" +
+                "  \"title\": \"Error in server room Again\",\n" +
+                "  \"description\": \"An error occurred in server room and everything is burning now. I'm super scared about it, need help!\",\n" +
+                "  \"dateTime\": \"2022-12-02T15:23:00\",\n" +
+                "  \"place\": \"Server Room\",\n" +
+                "  \"severity\":{\"value\":\"ALARM\"},\n" +
+                "  \"author\": \"Susan\"\n" +
+                "}";
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/api/events/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(MockMvcResultMatchers
+                        .status()
+                        .isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1l));
+    }
+    @Test
+    public void should_not_find_event_to_update() throws Exception {
+        Mockito.when(eventService.updateEvent(Mockito.eq(3l),Mockito.any(Event.class))).thenThrow(EventNotFoundException.class);
+
+        String json = "{\n" +
+                "  \"title\": \"Error in server room Again\",\n" +
+                "  \"description\": \"An error occurred in server room and everything is burning now. I'm super scared about it, need help!\",\n" +
+                "  \"dateTime\": \"2022-12-02T15:23:00\",\n" +
+                "  \"place\": \"Server Room\",\n" +
+                "  \"severity\":{\"value\":\"ALARM\"},\n" +
+                "  \"author\": \"Susan\"\n" +
+                "}";
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/api/events/3")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(MockMvcResultMatchers
+                        .status()
+                        .isNotFound());
+    }
 }
--- src/test/java/eventApp/apiServer/events/EventServicetest.java
+++ src/test/java/eventApp/apiServer/events/EventServicetest.java
@@ -1,17 +1,17 @@
 package eventApp.apiServer.events;
 
 import eventApp.apiServer.events.errors.EventNotFoundException;
-import lombok.SneakyThrows;
 import org.junit.jupiter.api.*;
-import org.mockito.InjectMocks;
 import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-import javax.swing.text.html.Option;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,11 +23,11 @@ public class EventServicetest {
     private EventRepository eventRepository;
     @Autowired
     private EventService eventService;
-
+    private List<Event> mockevs;
 
     @BeforeEach
     void setUp() throws EventNotFoundException {
-        List<Event> evs = new ArrayList<>(List.of(
+        this.mockevs = new ArrayList<>(List.of(
                 Event.builder()
                         .id(1L)
                         .author("Ben")
@@ -47,18 +47,20 @@ public class EventServicetest {
                         .severity(Severity.SUCCESS)
                         .build()
         ));
-        Mockito.when(eventRepository.findAll()).thenReturn(evs);
-        Mockito.when(eventRepository.getEventById(1l)).thenReturn(Optional.of(evs.get(0)));
-        Mockito.when(eventRepository.saveAll(evs)).thenReturn(evs);
-        Mockito.when(eventRepository.save(evs.get(0))).thenReturn(evs.get(0));
-        Mockito.when(eventRepository.deleteEventById(Mockito.anyLong())).thenReturn(Optional.of(evs.get(0)));
 
+        Mockito.when(eventRepository.findAll()).thenReturn(mockevs);
+        Mockito.when(eventRepository.findById(1l)).thenReturn(Optional.of(mockevs.get(0)));
+        Mockito.when(eventRepository.findById(3l)).thenReturn(Optional.empty());
+        Mockito.when(eventRepository.saveAll(Mockito.anyList())).thenReturn(mockevs);
+        Mockito.when(eventRepository.save(Mockito.any(Event.class))).thenReturn(mockevs.get(0));
+        Mockito.doNothing().when(eventRepository).delete(mockevs.get(0));
     }
 
     @Test
     void should_find_all_events(){
         List<Event> found = eventService.getEvents();
-        Assertions.assertTrue(found.size() == 2);
+        int mockevsSize = mockevs.size();
+        Assertions.assertTrue(found.size() == mockevsSize);
         Mockito.verify(eventRepository, Mockito.times(1)).findAll();
         Mockito.verifyNoMoreInteractions(eventRepository);
     }
@@ -68,18 +70,103 @@ public class EventServicetest {
     void should_find_and_return_one_event() throws EventNotFoundException {
         Event found = eventService.getEvent(1l);
         Assertions.assertEquals("Ben",found.getAuthor());
-        Mockito.verify(eventRepository, Mockito.times(1)).getEventById(1l);
+        Mockito.verify(eventRepository, Mockito.times(1)).findById(1l);
         Mockito.verifyNoMoreInteractions(eventRepository);
     }
 
 
     @Test
     void should_not_find_and_throw_exception() throws EventNotFoundException {
-       Mockito.when(eventRepository.getEventById(3l)).thenReturn(Optional.empty());
-
         Assertions.assertThrows(EventNotFoundException.class, () -> eventService.getEvent(3l));
 
-        Mockito.verify(eventRepository, Mockito.times(1)).getEventById(3l);
+        Mockito.verify(eventRepository, Mockito.times(1)).findById(3l);
         Mockito.verifyNoMoreInteractions(eventRepository);
+    }
+    @Test
+    void should_save_all_given_events() {
+
+        List<Event> toSave = List.of(
+                Event.builder()
+                        .author("Ben")
+                        .title("Error in server room")
+                        .place("Server Room")
+                        .description("An error occurred in server room and everython g is burning now. I'm super scared about it, need help!")
+                        .dateTime(LocalDateTime.of(2022,12,2,12,23))
+                        .severity(Severity.ALARM)
+                        .build(),
+                Event.builder()
+                        .author("John")
+                        .title("Clean completed")
+                        .place("2nd floor")
+                        .description("Cleaning operation was successfully completed at second floor, now moving at 3rd")
+                        .dateTime(LocalDateTime.of(2022,10,1,9,17))
+                        .severity(Severity.SUCCESS)
+                        .build()
+        );
+        List<Event> events = eventService.addEvents(toSave);
+        Assertions.assertTrue(events.size() == toSave.size());
+
+        Mockito.verify(eventRepository, Mockito.times(1)).saveAll(toSave);
+        Mockito.verifyNoMoreInteractions(eventRepository);
+    }
+    @Test
+    void should_save_given_event() {
+        Event toSave = Event.builder()
+                .author("Ben")
+                .title("Error in server room")
+                .place("Server Room")
+                .description("An error occurred in server room and everython g is burning now. I'm super scared about it, need help!")
+                .dateTime(LocalDateTime.of(2022,12,2,12,23))
+                .severity(Severity.ALARM)
+                .build();
+        Event ev = eventService.addEvent(toSave);
+        Assertions.assertEquals(1l,ev.getId());
+
+        Mockito.verify(eventRepository, Mockito.times(1)).save(toSave);
+        Mockito.verifyNoMoreInteractions(eventRepository);
+    }
+    @Test
+    void should_delete_event() throws EventNotFoundException {
+        Event toDelete = mockevs.get(0);
+        eventService.deleteEvent(toDelete.getId());
+
+        Mockito.verify(eventRepository, Mockito.times(1)).findById(toDelete.getId());
+        Mockito.verify(eventRepository, Mockito.times(1)).delete(toDelete);
+        Mockito.verifyNoMoreInteractions(eventRepository);
+    }
+
+    @Test
+    void should_not_delete_event_that_does_not_exist() throws EventNotFoundException {
+        Assertions.assertThrows(EventNotFoundException.class, () -> eventService.deleteEvent(3l));
+
+        Mockito.verify(eventRepository, Mockito.times(1)).findById(3l);
+        Mockito.verify(eventRepository, Mockito.times(0)).delete(Mockito.any(Event.class));
+        Mockito.verifyNoMoreInteractions(eventRepository);
+    }
+
+    @Test
+    void should_update_an_event() throws EventNotFoundException {
+        Event updatedEv = eventService.updateEvent(1l,Event.builder()
+                .author("Susan")
+                .title("Error in server room Again")
+                .place("Server Room")
+                .description("An error occurred in server room and everything is burning now. I'm super scared about it, need help!")
+                .dateTime(LocalDateTime.of(2022,12,2,15,23))
+                .severity(Severity.ALARM)
+                .build());
+        Assertions.assertEquals(1l,updatedEv.getId());
+        Assertions.assertEquals("Susan",updatedEv.getAuthor());
+    }
+    @Test
+    void should_not_update_event_that_does_not_exist() throws EventNotFoundException {
+        Event toUpdateEv = Event.builder()
+                .author("Susan")
+                .title("Error in server room Again")
+                .place("Server Room")
+                .description("An error occurred in server room and everything is burning now. I'm super scared about it, need help!")
+                .dateTime(LocalDateTime.of(2022,12,2,15,23))
+                .severity(Severity.ALARM)
+                .build();
+        Assertions.assertThrows(EventNotFoundException.class, () -> eventService.updateEvent(3l, toUpdateEv));
     }
 }
